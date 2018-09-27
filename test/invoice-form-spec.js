69bcacb49f31a688bcd44f82464f85df54557670
AkrasiDalin <dalin.jo@yahoo.com>
2018-09-27 13:22:27 +0100

    Create first test

---
+++ test/invoice-form-spec.js
@@ -0,0 +1,11 @@
+const chai = require('chai');
+const assert = chai.assert;
+const expect = chai.expect;
+const should = chai.should();
+
+
+describe('invoice data', () => {
+    it('should make a copy of data input in text field', () => {
+        assert.equal(1,2);
+    })
+});
